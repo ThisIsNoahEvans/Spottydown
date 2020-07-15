68edca6d5ee811fc28e3d4efbb02be3cf8e522ec
Noah Evans <noah@itsnoahevans.co.uk>
2020-07-15 19:36:39 +0100

    Initial Commit

---
+++ Spottydown/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Spottydown
+//
+//  Created by Noah Evans on 15/07/2020.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Spottydown/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Spottydown
+//
+//  Created by Noah Evans on 15/07/2020.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
