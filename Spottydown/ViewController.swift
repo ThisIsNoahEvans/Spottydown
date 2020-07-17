5ebef223e44fd2c085d414d3aac19f82f39219d9
Noah Evans <noah@itsnoahevans.co.uk>
2020-07-17 15:03:56 +0100

    First working beta build. Release soon!

--- Spottydown/ViewController.swift
+++ Spottydown/ViewController.swift
@@ -5,10 +5,20 @@
 //  Created by Noah Evans on 15/07/2020.
 //
 
+
 import Cocoa
+import Foundation
+
+
+
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var textField: NSTextField!
+    
+    @IBOutlet weak var locField: NSTextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,7 +31,41 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
+    
+    @IBAction func download(_ sender: Any) {
+        let task = Process()
+        let command = "spotdl -s \"\(textField.stringValue)\" -f \"\(locField.stringValue)\""
+        task.executableURL = URL(fileURLWithPath: "/bin/zsh")
+        task.arguments = ["--login", "-c", command]
 
+        let connection = Pipe()
+        task.standardOutput = connection
+
+        do {
+            try task.run()
+        }
+        catch {
+            print("Error")
+        }
+    }
+    
 
 }
 
+@discardableResult
+private func shell(_ args: String) -> String {
+    var outstr = ""
+    let task = Process()
+    task.launchPath = "/bin/zsh`"
+    task.arguments = ["--login", "-c", args]
+    let pipe = Pipe()
+    task.standardOutput = pipe
+    task.launch()
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    if let output = String(data: data, encoding: .utf8) {
+        outstr = output as String
+    }
+    task.waitUntilExit()
+    return outstr
+}
